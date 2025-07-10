fc0ee53fc3cca0c282aec93a43c2dd9dd4537330
kagawa_shiori <kagawa.shiori@alhinc.jp>
2025-07-10 18:26:32 +0900

    エラー処理の追加

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +24,8 @@ public class CalculateSales {
 	private static final String UNKNOWN_ERROR = "予期せぬエラーが発生しました";
 	private static final String FILE_NOT_EXIST = "支店定義ファイルが存在しません";
 	private static final String FILE_INVALID_FORMAT = "支店定義ファイルのフォーマットが不正です";
+	private static final String NOT_CONSECUTIVE_NUMBERS = "売上ファイル名が連番になっていません";
+	private static final String TOTAL_AMOUNT_EXCEEDED_10_DIGITS = "合計金額が10桁を超えました";
 
 	/**
 	 * メインメソッド
@@ -34,6 +37,12 @@ public class CalculateSales {
 		Map<String, String> branchNames = new HashMap<>();
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
+
+		//エラー処理3-1 コマンドライン引数が1つ設定されているかチェック
+		if(args.length != 1) {
+			System.out.println(UNKNOWN_ERROR);
+			return;
+		}
 
 		// 支店定義ファイル読み込み処理
 		if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
@@ -56,22 +65,51 @@ public class CalculateSales {
 
 			//.isFile→ファイルかどうかを調べる
 			//拡張子rcdかつファイル名が数字8桁のファイル名だったら、List(ArrayList)に追加する
-			if(files[i].isFile() && fileName.matches("^[0-9]{8}.rcd$")) {
+			//正規表現　.の部分　そのままだと設定が甘いので、修正(.は任意の1文字にマッチするメタ文字のため)
+			//"[.]"や"\\."で設定してあげるとよい
+			//エラー処理3-2 売上ファイルがファイルか(ディレクトリじゃないか)を確認
+			//isFileメソッドで、ファイルの場合はtrue、それ以外の場合はfalseを返す
+			if(files[i].isFile() && fileName.matches("^[0-9]{8}\\.rcd$")) {
 				rcdFiles.add(files[i]);
 			}
 		}
 
-		//支店定義ファイル読み込み
+		Collections.sort(rcdFiles);
+
+		//エラー処理2-1
+		//売上ファイルが連番かチェック
+		//比較回数は売上ファイルの数よりも1回少ないため、売上ファイルのリストの数-1回繰り返す
+		for(int i = 0; i < rcdFiles.size() -1; i++) {
+			//売上ファイルのうち若い数字の方を格納する変数を準備
+			//売上ファイル名は、rcdFiles.get(i)→i番目のrcdFiles .getName()→その売上ファイルの名前 で取得できる
+			//Substringメソッドで、引数で指定した範囲の文字列を切り出して返す(今回の場合、先頭の数字8文字)
+			//.substringのままだと文字列なので、parseIntを使って型変換する
+			int former = Integer.parseInt(rcdFiles.get(i).getName().substring(0, 8));
+			//売上ファイルのうち若くない数字の方を格納する変数を準備
+			int latter = Integer.parseInt(rcdFiles.get(i + 1).getName().substring(0, 8));
+
+			//若くない方と若い方の数字を比較して、差が1じゃないかどうかを判定
+			if((latter - former) != 1) {
+				//差が1でないなら想定外なので、エラーメッセージを表示する
+				System.out.println(NOT_CONSECUTIVE_NUMBERS);
+				return;
+			}
+		}
+
+		//売上ファイル読み込み
 		BufferedReader br = null;
 
 		//rcdFilesに複数の売上ファイルを格納しているので、その数だけ処理を繰り返す
 		for(int i = 0; i < rcdFiles.size(); i++) {
 			try {
 				//売上ファイル「rcdFiles」を読み込む
-				br = new BufferedReader(new FileReader(rcdFiles.get(i)));
+				FileReader fr = new FileReader(rcdFiles.get(i));
+				br = new BufferedReader(fr);
+				//br = new BufferedReader(new FileReader(rcdFiles.get(i)));
 
-				//売上金額を入れるためのリスト「fileContents」を作成する→ここの理解が抜けてた
+				//売上ファイルの中身を入れるためのリスト「fileContents」を作成する→ここの理解が抜けてた
 				ArrayList<String> fileContents = new ArrayList<>();
+
 				//rcdFilesの1行目に支店コード、2行目には売上金額が入っている
 				//最後の行まで読み込み、fileContentsに要素を1つずつ追加していく
 				String line = "";
@@ -79,17 +117,59 @@ public class CalculateSales {
 					fileContents.add(line);
 				}
 
+				//行数チェック、支店コードチェックの際に対象売上ファイル名を表現するための変数を準備
+				String fileName = rcdFiles.get(i).getName();
+
+				//エラー処理2-4.
+				//売上ファイルフォーマットの行数チェック(1行目に支店コード、2行目に売上金額が記載されているか)
+				if(fileContents.size() != 2){
+					System.out.println(fileName +"のフォーマットが不正です");
+					return;
+				}
+
 				//branchCode変数に、fileContentsリストの1つめの要素、つまり支店コードを入れる
 				//branchCodeは、後でMapの支店コードをKeyにValueを取り出す際に使用する
 				String branchCode = fileContents.get(0);
+
+				//エラー処理2-3. 支店コードのチェック
+				//売上ファイルの支店コードが支店定義ファイルに存在するか確認
+				//つまり支店情報を保持しているMap「branchNames」に、売上ファイルの支店コードが存在しているか確認
+				//containsKeyメソッドで、引数で指定したKeyがMapに存在する場合はtrue,存在しない場合はfalseを返す
+				if(!branchNames.containsKey(branchCode)) {
+					//売上ファイルの支店コードが存在していなかったらエラーメッセージを表示する
+					System.out.println(fileName + "の支店コードが不正です");
+					return;
+				}
+
+				//エラー処理3-3 売上金額が数字なのかチェック
+				//売上金額を読み込む際に、売上金額をLongに変換するため、売上金額が数字なのかを事前に確認する必要がある
+				//^[0-9]+$: 123, 5, 0 など、文字列全体が1つ以上の数字で構成されている場合にマッチする
+				//^\d+$ でも表せるが、\dはUnicodeの数字にもマッチする可能性があり、半角数字のみに限定したい場合は不向き
+				//+ の代わりに * を使用すると、0回以上の繰り返し＝空文字列もマッチしてしまうので非推奨
+				if(!fileContents.get(1).matches("^[0-9]+$")) {
+					System.out.println(UNKNOWN_ERROR);
+					return;
+				}
 
 				//ファイルから読み込んだ情報は一律で文字列として扱われるので、Long型に変換する
 				//long型のfileSale変数に、fileContentsリストの2つめの要素、つまり売上金額を入れる
 				long fileSale = Long.parseLong(fileContents.get(1));
 
+
 				//読み込んだ売上金額を加算する
 				//Map「branchSales」のKey(支店コード)を渡してバリュー(売上金額)を取り出し、longに変換した売上金額(foleSale)を加算する
 				Long saleAmount = branchSales.get(branchCode) + fileSale;
+
+				//エラー処理2-2. 売上金額の合計が11桁以上になっていないかチェック
+				if(saleAmount >= 10000000000L) {
+					//売上金額が11桁以上の場合はエラーメッセージを表示する
+					System.out.println(TOTAL_AMOUNT_EXCEEDED_10_DIGITS);
+					return;
+				}
+
+
+
+
 
 				//加算した売上金額をMap「branchSales」に追加
 				branchSales.put(branchCode, saleAmount);
@@ -134,6 +214,14 @@ public class CalculateSales {
 
 		try {
 			File file = new File(path, fileName);
+
+			//エラー処理1-1 支店定義ファイルが存在しない場合、エラー出力
+			if(!file.exists()) {
+				System.out.println(FILE_NOT_EXIST);
+				//falseを返すようにする
+				return false;
+			}
+
 			FileReader fr = new FileReader(file);
 			br = new BufferedReader(fr);
 
@@ -145,6 +233,16 @@ public class CalculateSales {
 				// 処理内容1-2
 				//splitにて文字列をカンマで分割
 				items = line.split(",");
+
+				//エラー処理1-2 支店定義ファイルフォーマットチェック
+				//1行に支店コードと支店名が「,」で区切られて記載されているか、items配列の要素の長さでチェック
+				//支店コードが数字3桁かをチェック
+				if((items.length != 2) || (!items[0].matches("^[0-9]{3}$"))){
+					System.out.println(FILE_INVALID_FORMAT);
+					//falseを返すようにする
+					return false;
+				}
+
 
 				//Map「branchNames」に支店コードと支店名を追加する
 				//支店コードは配列の0番目、支店名は配列の1番目に入っている
@@ -207,7 +305,7 @@ public class CalculateSales {
 			System.out.println(UNKNOWN_ERROR);
 			return false;
 		} finally {
-			// 書き込みファイルがnull出ない場合
+			// 書き込みファイルがnullでない場合
 			if(bw != null) {
 				try {
 					//closeメソッドでファイルへのデータ出力の流れを止める
